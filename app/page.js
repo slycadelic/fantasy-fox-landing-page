80875ed81dde65227c1bafc14ba56b54d0f2344b
Sattyam Samania <67833888+SattyamSamania@users.noreply.github.com>
2023-12-27 14:14:53 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import BannerTitle from '@/components/BannerTitle';
-
+import Rules from '@/components/Rules';
 export default function Page() {
     return (
         <div className='mainContainer'>
@@ -22,29 +22,29 @@ export default function Page() {
                     />
                 </div>
 
-                {/* Links */}
-                <div
+                {/* <div
                     className='Main-Links'
                 >
-                    {/* Web App Link */}
                     <a href="/web-app-link" className='links'>
                         <button className='web-app'>
                             Web App
                         </button>
                     </a>
-                    {/* Play Store Link */}
                     <a href="/play-store-link" className='links'>
                         <button className='play-store'>
                             Play Store
                         </button>
                     </a>
-                    {/* App Store Link */}
                     <a href="/app-store-link" className='links'>
                         <button className='app-store'>
                             App Store
                         </button>
                     </a>
-                </div>
+                </div> */}
+
+              <Rules/>
+          
+          
             </div>
         </div>
     );
