116c40de4ee6bd8d793dca8f78341db3dcebd041
SattyamSamania <Sattyamsamania2810@gmail.com>
2023-12-28 10:54:35 +0530

    Updated How2Play Component

--- components/home/How2Play.js
+++ components/home/How2Play.js
@@ -1,27 +1,29 @@
+import styles from './How2Play.module.css'
+
 const How2Play = () => {
     return (
         <>
             {/* How to Play Section */}
-            <div className='how-to-play'>
+            <div className={styles.howToPlay}>
                 <h2>How to Play</h2>
-                <div className='steps'>
-                    <div className='step'>
-                        <img src='/step1-icon.png' alt='Step 1 Icon' className='step-icon' />
+                <div className={styles.steps}>
+                    <div className={styles.step}>
+                        <img src='/step1-icon.png' alt='Step 1 Icon' className={styles.stepIcon} />
                         <h3>Step 1</h3>
                         <p>Description of step 1</p>
                     </div>
-                    <div className='step'>
-                        <img src='/step2-icon.png' alt='Step 2 Icon' className='step-icon' />
+                    <div className={styles.step}>
+                        <img src='/step2-icon.png' alt='Step 2 Icon' className={styles.stepIcon} />
                         <h3>Step 2</h3>
                         <p>Description of step 2</p>
                     </div>
-                    <div className='step'>
-                        <img src='/step3-icon.png' alt='Step 3 Icon' className='step-icon' />
+                    <div className={styles.step}>
+                        <img src='/step3-icon.png' alt='Step 3 Icon' className={styles.stepIcon} />
                         <h3>Step 3</h3>
                         <p>Description of step 3</p>
                     </div>
-                    <div className='step'>
-                        <img src='/step4-icon.png' alt='Step 4 Icon' className='step-icon' />
+                    <div className={styles.step}>
+                        <img src='/step4-icon.png' alt='Step 4 Icon' className={styles.stepIcon} />
                         <h3>Step 4</h3>
                         <p>Description of step 4</p>
                     </div>
