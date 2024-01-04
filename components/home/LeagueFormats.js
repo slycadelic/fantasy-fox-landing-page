1540dd1de0c45bc7af7e4b31b299b55de6117e20
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-01-04 15:19:55 +0530

    Updated LeagueFormat and wiki.md file

--- components/home/LeagueFormats.js
+++ components/home/LeagueFormats.js
@@ -25,23 +25,23 @@ const LeagueFormat = () => {
             <nav>
                 <ul className={styles.nav}>
                     <li className={styles.li}>
-                        <button onClick={() => scrollToElement(step1Ref.current)} className={styles.btn}> Soft
+                        <button onClick={() => scrollToElement(step1Ref.current)} className={styles.btn}> <span className='text-[rgb(255,53,53)] '>Soft</span>
                         </button>
                     </li>
                     <li className={styles.li}>
-                        <button onClick={() => scrollToElement(step2Ref.current)} className={styles.btn}>Step2
+                        <button onClick={() => scrollToElement(step2Ref.current)} className={styles.btn}><span className='text-[rgb(255,255,0)]'>Medium</span>
                         </button>
                     </li>
                     <li className={styles.li}>
-                        <button onClick={() => scrollToElement(step3Ref.current)} className={styles.btn}> Step3
+                        <button onClick={() => scrollToElement(step3Ref.current)} className={styles.btn}><span className='text-[rgb(205,205,205)]'>Hard</span>
                         </button>
                     </li>
                     <li className={styles.li}>
-                        <button onClick={() => scrollToElement(step4Ref.current)} className={styles.btn}>Step4
+                        <button onClick={() => scrollToElement(step4Ref.current)} className={styles.btn}><span className='text-[rgb(49,145,255)]'>Custom</span>
                         </button>
                     </li>
                     <li className={styles.li}>
-                        <button onClick={() => scrollToElement(step5Ref.current)} className={styles.btn}>Step5
+                        <button onClick={() => scrollToElement(step5Ref.current)} className={styles.btn}><span className='text-[rgb(255,100,0)]'>Contest</span>
                         </button>
                     </li>
                 </ul>
@@ -49,26 +49,38 @@ const LeagueFormat = () => {
             <div className={styles.steps}>
                 <div className={styles.scrollContainer}>
                     <div ref={step1Ref} className={styles.step}>
-                        <p>Soft Format</p>
+                        <h3>Soft Format</h3>
                         The Soft Format is a pre-defined league format that has a budget based selection strategy; it allows users to pick drivers while staying within a fixed allocated budget of $100M. Driver Prices are set based on their performance.
                         The Scoring system, Bonus Points, wildcards and transfer rules in this format are standard. (Details can be found in the Rules section).
                         A team created in this format can join multiple leagues of the same format.
                     </div>
                     <div ref={step2Ref} className={styles.step}>
-                        <h3>Step 2</h3>
-                        <p>Description of step 2</p>
+                        <h3>Medium Format</h3>
+                        <p>The Medium Format is a pre-defined league format that has a contracts based selection strategy; allocates race contracts for all drivers (based on the number of races). Users use the race contracts to pick drivers for each race and are forced to transfer them out when a driver runs out of race contracts.
+Failing to transfer them out will result in no points being awarded for that driver.
+The Scoring system, Bonus Points, wildcards and transfer rules in this format are standard. (Details can be found in the Rules section).
+A team created in this format can join multiple leagues of the same format</p>
                     </div>
                     <div ref={step3Ref} className={styles.step}>
-                        <h3>Step 3</h3>
-                        <p>Description of step 3</p>
+                        <h3>Hard Format</h3>
+                        <p>The Hard Format is a pre-defined league format that takes both contracts and budget into account for the selection strategy.
+Therefore, while users have to stay within the budget of $100M, they are also required to use race contracts to pick drivers in each race; and not be allowed to pick drivers with no contracts left or go over the budget at any point.
+The Scoring system, Bonus Points, wildcards and transfer rules in this format are standard. (Details can be found in the Rules section).
+A team created in this format can join multiple leagues of the same format.</p>
                     </div>
                     <div ref={step4Ref} className={styles.step}>
-                        <h3>Step 4</h3>
-                        <p>Description of step 4</p>
+                        <h3>Custom Format</h3>
+                        <p>The Hard Format is a pre-defined league format that takes both contracts and budget into account for the selection strategy.
+Therefore, while users have to stay within the budget of $100M, they are also required to use race contracts to pick drivers in each race; and not be allowed to pick drivers with no contracts left or go over the budget at any point.
+The Scoring system, Bonus Points, wildcards and transfer rules in this format are standard. (Details can be found in the Rules section).
+A team created in this format can join multiple leagues of the same format.</p>
                     </div>
                     <div ref={step5Ref} className={styles.step}>
-                        <h3>Step 5</h3>
-                        <p>Description of step 5</p>
+                        <h3>Contest Format</h3>
+                        <p>The Contest League is a special type of global league that is created for every race and is applicable for that round only.
+The league format is similar to the Soft format where you have a budget to create a team. The scoring system is standard but no chips are available to use.
+Joining a contest league for a round requires creating a new team for that round. Joining other leagues with that team is not permitted.
+Since the contest league is different for each round, creating a new team is required for each round and selections from the previous rounds are not applicable.</p>
                     </div>
                 </div>
             </div>
--- wiki.md
+++ wiki.md
@@ -11,5 +11,43 @@ npm install --save-dev @svgr/webpack
 yarn add --dev @svgr/webpack
 
 ## How to Add Dynamic background to Navbar.
+- For Adding the Dynamic Background We have Used useWindowScroll Hook. 
+
+- This custom React hook tracks the vertical scroll position of the window. It uses the useState and useEffect hooks to manage and update the scroll position state.
+
+# How to Use useWindowScroll Hook?
+
+1. Initialization: Initializes state using the useState hook to hold the vertical scroll position (scrollY) with an initial value of 0.
+
+
+2.Event Listener on Window Scroll:
+
+- Uses useEffect to handle side effects within the component.
+- Adds an event listener to the scroll event of the window.
+- Defines a handleScroll function that updates the scrollY state with the current vertical scroll position (window.scrollY).
+
+3. Cleanup and Unmounting:
+
+- Removes the event listener on cleanup by returning a cleanup function from useEffect.
+- Ensures the event listener is removed when the component using the hook is unmounted to prevent memory leaks.
+
+4. Return Value:
+
+Returns the scrollY state, representing the vertical scroll position of the window.
+
+# Usage 
+ <!-- const scrollY = useWindowScroll();
+    const backgroundColor = `rgba(0,0,0,${scrollY / 1000})`; -->
+
+- As shown in the above code we have used scrollY from the useWindowScroll Hook and changed the background color using that.
+- So, as the user scrolls, the backgroundColor variable will change dynamically based on the vertical scroll position, transitioning from an almost transparent color (alpha = 0) to a more opaque color (alpha = 1) as scrollY increases.
 
 ## How to use scrollIntoView for horizontal navigation in a component
+
+- For Creating Horizontal navigation we have used useRef Hook to create references to different elements in the DOM.
+
+- Firstly we have created References for all the Steps required using the useRef Hook.
+- THe scrollIntoView Funtion is used to scroll smoothly to that element within a container, adjusting the scrolling behavior.
+- We can adjust different parameters like behavior, inline and block according to the Scrolling behaviour.
+- FOr Triggering the Scrolling behaviour we have to pass an onClick funtion to all the buttons where we want the Horizontal Effect.
+- Finally we have to pass the Reference in every div element so that they can hold the information when the scrolling occurs.
